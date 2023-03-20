e2a843c5e2eccf0d21de9b50956f1f75b7044e08
josunghoon <josunghoon@localhost>
2023-03-20 13:40:03 +0900

    cors 설정

--- security/src/main/java/com/project/security/config/SecurityConfig.java
+++ security/src/main/java/com/project/security/config/SecurityConfig.java
@@ -12,6 +12,10 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.project.security.jwt.JwtAuthenticationFilter;
 import com.project.security.jwt.JwtTokenProvider;
@@ -21,10 +25,22 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer{
 	
 	private final JwtTokenProvider jwtTokenProvider;
 	private final RedisTemplate redisTemplate;
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration corsConfiguration = new CorsConfiguration();
+	    corsConfiguration.addAllowedOrigin("http://localhost:3000");
+	    corsConfiguration.addAllowedMethod("*");
+	    corsConfiguration.addAllowedHeader("*");
+	    corsConfiguration.setAllowCredentials(true);
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", corsConfiguration);
+	    return source;
+	}
 	
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -56,5 +72,5 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
+    
 }
